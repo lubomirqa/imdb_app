5e08f04f03570d23a75d10e7b661524fbd3ab050
Lubomir <lubomir.peretyatko@gmail.com>
2020-08-04 01:43:09 +0300

    .gitignore update

---
+++ src/logic/Movie.java
@@ -0,0 +1,58 @@
+package logic;
+
+import data.Database;
+import java.util.ArrayList;
+
+public class Movie {
+  private String primaryTitle;
+  private Integer startYear;
+  private Float averageRating;
+  private Integer numVotes;
+
+  public Movie(String pt, Integer sy, Float ar, Integer nv){
+    primaryTitle = pt;
+    startYear = sy;
+    averageRating = ar;
+    numVotes = nv;
+  }
+
+  public static ArrayList<Movie> findMovies(Integer minMovies, String titleType, String genre){
+    return Database.findMovies(minMovies, titleType, genre);
+  }
+
+  public static ArrayList<Movie> findMovies(String title, Integer minMovies, String titleType, String genre){
+    return Database.findMovies(title, minMovies, titleType, genre);
+  }
+
+  public String getPrimaryTitle() {
+    return primaryTitle;
+  }
+
+  public void setPrimaryTitle(String primaryTitle) {
+    this.primaryTitle = primaryTitle;
+  }
+
+  public Integer getStartYear() {
+    return startYear;
+  }
+
+  public void setStartYear(Integer startYear) {
+    this.startYear = startYear;
+  }
+
+  public Float getAverageRating() {
+    return averageRating;
+  }
+
+  public void setAverageRating(Float averageRating) {
+    this.averageRating = averageRating;
+  }
+
+  public Integer getNumVotes() {
+    return numVotes;
+  }
+
+  public void setNumVotes(Integer numVotes) {
+    this.numVotes = numVotes;
+  }
+}
